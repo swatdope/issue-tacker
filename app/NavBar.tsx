3420987e84212dea49542bd503c366e44fb8628d
Ngudup <tenzinngodup900@gmail.com>
2024-01-27 00:04:04 +0530

    Build the Navbar

---
+++ app/NavBar.tsx
@@ -0,0 +1,31 @@
+import Link from "next/link";
+import React from "react";
+import { AiFillBug } from "react-icons/ai";
+
+const NavBar = () => {
+  const links = [
+    { lable: "Dashboard", href: "/" },
+    { lable: "Isues", href: "/" },
+  ];
+
+  return (
+    <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center ">
+      <Link href="/">
+        <AiFillBug />
+      </Link>
+      <ul className="flex space-x-6 ">
+        {links.map((link) => (
+          <Link
+            key={link.href}
+            className="text-zinc-500 hover:text-zinc-700 transition-colors"
+            href="/"
+          >
+            {link.lable}
+          </Link>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
