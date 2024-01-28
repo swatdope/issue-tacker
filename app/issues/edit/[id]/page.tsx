6ac0af315d8d64a61549a96e1e5e9ed74acbd0a2
Ngudup <tenzinngodup900@gmail.com>
2024-01-28 19:44:49 +0530

    Build the filter component

--- app/issues/edit/[id]/page.tsx
+++ app/issues/edit/[id]/page.tsx
@@ -5,22 +5,22 @@ import dynamic from "next/dynamic";
 import IssueFormSkeleton from "./loading";
 
 const IssueForm = dynamic(() => import("@/app/issues/_components/IssueForm"), {
-  ssr: false,
-  loading: () => <IssueFormSkeleton />,
+    ssr: false,
+    loading: () => <IssueFormSkeleton />,
 });
 
 interface Props {
-  params: { id: string };
+    params: { id: string };
 }
 
 const EditIssuePage = async ({ params }: Props) => {
-  const issue = await prisma.issue.findUnique({
-    where: { id: parseInt(params.id) },
-  });
+    const issue = await prisma.issue.findUnique({
+        where: { id: parseInt(params.id) },
+    });
 
-  if (!issue) notFound();
+    if (!issue) notFound();
 
-  return <IssueForm issue={issue} />;
+    return <IssueForm issue={issue} />;
 };
 
 export default EditIssuePage;
